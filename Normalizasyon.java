a937f980e79ae89b184570fbfc83cb2c2f6aaf32
Metehan Durmuş <59119987+metehandurmus@users.noreply.github.com>
2020-10-19 20:29:59 +0300

    Create Normalizasyon.java

---
+++ Normalizasyon.java
@@ -0,0 +1,44 @@
+// Veri dizilerine normalizasyon uygulayan java kodu
+
+package javaornekler;
+
+import java.util.Scanner;
+
+public class Normalizasyon {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Kaç veri girilecek : ");
+        int x = input.nextInt();
+        double[] veri = new double[x];
+        for (int i = 0; i < veri.length; i++) {
+            System.out.print("a["+i+"] = ");
+            veri[i] = input.nextDouble();
+        }
+        Norm(veri);
+    }
+    
+    public static void Norm(double[] a) {
+        int varyans = 0, i, j;
+        double ortalama = 0, stdSapma, temp;
+        for (i = 1; i < a.length; i++) {
+            for (j = 0; j < a.length - 1; j++) {
+                if (a[j] > a[j+1]) {
+                    temp = a[j];
+                    a[j] = a[j+1];
+                    a[j+1] = temp;
+                }
+            }
+        }
+        for (i = 0; i < a.length; i++)
+            ortalama += a[i];
+        ortalama /= a.length;
+        for (i = 0; i < a.length; i++)
+            varyans += Math.pow((a[i] - ortalama), 2) / (a.length - 1);
+        stdSapma = Math.sqrt(varyans);
+        for (i = 0; i < a.length; i++)
+            a[i] = (a[i] - ortalama) / stdSapma;
+        for (i = 0; i < a.length; i++)
+            System.out.println(a[i]);
+    }
+}
